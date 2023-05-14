8903e3cb7fd0f3e9ec42c84003ea6c2d7a164ca8
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-05-14 13:42:44 +0700

    add navtab

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import SplashScreen from "./components/SplashScreen";
-import { Header, Icon } from "@rneui/native";
-import { BottomNavigation, BottomNavigationTab } from '@rneui/native';
-import { Icon } from 'react-native-elements';
-
+import { BottomNavigation, BottomNavigationTab } from "react-native-elements";
+import { NavigationContainer } from "@react-navigation/native";
+import NavigationTab from "./components/NavigationTab";
+// import { Icon } from 'react-native-elements';
 
 const App = () => {
   const [showSplashScreen, setShowSplashScreen] = useState(true);
@@ -14,62 +14,20 @@ const App = () => {
       setShowSplashScreen(false);
     }, 3000); // Change the value to adjust the length of time the splash screen is displayed
   }, []);
-
-  const renderView = (index) => {
-    switch (index) {
-      case 0:
-        return (
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
-            <Text>Home Screen</Text>
-          </View>
-        );
-      case 1:
-        return (
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
-            <Text>Settings Screen</Text>
-          </View>
-        );
-      case 2:
-        return (
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
-            <Text>Profile Screen</Text>
-          </View>
-        );
-      default:
-        return null;
-    }
-  };
-
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-
-  const onTabSelect = (index) => {
-    setSelectedIndex(index);
-  };
-
   return (
-    <View style={{ flex: 1 }}>
-      {showSplashScreen ? (
-        <SplashScreen />
-      ) : (
-        // Render your main component here
-        <Text>Hello, world!</Text>
-      )}
+    <NavigationContainer>
+      <View style={{ flex: 1 }}>
+        {showSplashScreen ? (
+          <SplashScreen />
+        ) : (
+          // Render your main component here
+          <View style={{ flex: 1 }}>
+            <NavigationTab />
+          </View>
+        )}
 
-      {renderView(selectedIndex)}
-      <BottomNavigation
-        selectedIndex={selectedIndex}
-        onSelect={onTabSelect}
-        style={{ backgroundColor: "#3D6DCC" }}
-      >
-        {navigationTabs.map(renderTab)}
-      </BottomNavigation>
-    </View>
+      </View>
+    </NavigationContainer>
   );
 };
 
---
+++ components/NavigationTab.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+};
+
+const YourPlanScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+};
+
+const CreateScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Create Screen</Text>
+    </View>
+  );
+};
+
+const NavigationTab = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'your-plan') {
+            iconName = focused ? 'calendar' : 'calendar-outline';
+          } else if (route.name === 'create') {
+            iconName = focused ? 'pencil' : 'pencil-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="home" options={{title: "Home"}} component={HomeScreen} />
+      <Tab.Screen name="create" options={{title: "Create"}} component={CreateScreen} />
+      <Tab.Screen name="your-plan" options={{title: "Your Plan"}} component={YourPlanScreen} />
+      <Tab.Screen name="profile" options={{title: "Profile"}} component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default NavigationTab;
